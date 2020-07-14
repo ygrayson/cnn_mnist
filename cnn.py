9225582aa2ab0e20cd5ee886a73c45c3edcaf292
Qianbo Yin <graysonyin717@gmail.com>
2020-07-14 13:55:49 +0800

    load data

--- cnn.py
+++ cnn.py
@@ -10,7 +10,7 @@ import torch.utils.data
 from torch.autograd import Variable
 
 import torchvision
-import torchvision.datasets.MNIST as MNIST
+from torchvision.datasets import MNIST
 
 
 class CNN(nn.Module):
@@ -26,7 +26,10 @@ def main():
     # 首先载入数据，这里直接使用PyTorch的MNIST数据集
     train_data = MNIST(root='./data', train=True, download=True, transform=None)
     test_data = MNIST(root='./data', train=False, download=True, transform=None)
-    print(train_data.shape())
+    #print(train_data)
+    #print(test_data)
+
+    
 
 
 if __name__ == '__main__':
